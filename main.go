3b24481a6488fca55ceb465589fe6dbc21577001
xrstf <git@xrstf.de>
2020-01-28 14:56:20 +0100

    allow to specify an instance name

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
 	"net/http"
@@ -9,19 +10,29 @@ import (
 	"strconv"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	log.Printf(`[%s] "%s %s" %s "%s"`, r.RemoteAddr, r.Method, r.URL, r.Proto, r.UserAgent())
+func newHandler(instance string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if instance != "" {
+			log.Printf(`[%s] [%s] "%s %s" %s "%s"`, instance, r.RemoteAddr, r.Method, r.URL, r.Proto, r.UserAgent())
+		} else {
+			log.Printf(`[%s] "%s %s" %s "%s"`, r.RemoteAddr, r.Method, r.URL, r.Proto, r.UserAgent())
+		}
 
-	response, err := httputil.DumpRequest(r, true)
-	if err != nil {
-		log.Println(err)
+		response, err := httputil.DumpRequest(r, true)
+		if err != nil {
+			log.Println(err)
 
-		w.WriteHeader(500)
-		response = []byte("Could not dump incoming request.\n")
+			w.WriteHeader(500)
+			response = []byte("Could not dump incoming request.\n")
+		}
+
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.Write(response)
+
+		if instance != "" {
+			w.Write([]byte(fmt.Sprintf("Instance: %s\n", instance)))
+		}
 	}
-
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.Write(response)
 }
 
 func main() {
@@ -35,6 +46,8 @@ func main() {
 		listenPort = "8080"
 	}
 
+	instance := os.Getenv("INSTANCE")
+
 	port, err := strconv.Atoi(listenPort)
 	if err != nil {
 		log.Fatal(err)
@@ -42,7 +55,7 @@ func main() {
 
 	log.Printf("Listening on %s:%d …\n", listenOn, port)
 
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", newHandler(instance))
 	err = http.ListenAndServe(net.JoinHostPort(listenOn, strconv.Itoa(port)), nil)
 	if err != nil {
 		log.Fatal(err)
